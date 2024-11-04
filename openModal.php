004da8f875a70e78d86e2fc9a9f2edfc17334d37
Jesús Acuña <jesuscheoz@gmail.com>
2024-11-04 00:22:32 -0300

    Se termino de crear el modal

--- openModal.php
+++ openModal.php
@@ -3,10 +3,55 @@
         <div class="modal-content">
             <div class="modal-header">
                 <center><h4 class="modal-title">Agregar Contacto</h4></center>
-                <button type="button" class="close" data-dismess="modal" aria-label="Close">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
             </div>
+            <div class="modal-body">
+                <div class="container-fluid">
+                    <form action="agregar.php" method="post">
+                        <div class="row form-group">
+                            <div class="col-sm-2">
+                                <label class="control-label">Nombre:</label>
+                            </div>
+                            <div class="col-sm-10">
+                                <input type="text" class="form-control" name="nombreContacto">
+                            </div>
+                        </div>
+                        <div class="row form-group">
+                            <div class="col-sm-2">
+                                <label class="control-label">Telefono:</label>
+                            </div>
+                            <div class="col-sm-10">
+                                <input type="tel" class="form-control" name="telefonoContacto">
+                            </div>
+                        </div>
+                        <div class="row form-group">
+                            <div class="col-sm-2">
+                                <label class="control-label">Correo:</label>
+                            </div>
+                            <div class="col-sm-10">
+                                <input type="email" class="form-control" name="emailContacto">
+                            </div>
+                        </div>
+                        <div class="row form-group">
+                            <div class="col-sm-2">
+                                <label class="control-label">Dirección:</label>
+                            </div>
+                            <div class="col-sm-10">
+                                <input type="text" class="form-control" name="direccionContacto">
+                            </div>
+                        </div>
+
+                     <div class="modal-footer">
+                        <button class="btn btn-primary" type="submit" name="guardar"><i class="fa fa-save"></i> Guardar</button> 
+                        <button class="btn btn-danger" type="button" data-dismiss="modal"><i class="fa fa-times"></i> Cerrar</button></button>
+                     </div>
+
+                    </form>
+                </div>
+            </div>
         </div>
+       
     </div>
 </div>
